ad29a99959d9283222ce083dee28431fda60bc89
sanjiv kannaa jeganathan <jsanjiv2003@gmail.com>
2022-12-21 22:15:37 +0530

    update

---
+++ codechef starters/70/6_frequency_array_retrival.py
@@ -0,0 +1,41 @@
+for __ in range(int(input())):
+    n = int(input())
+    b = [int(i) for i in input().split()]
+    d = dict()
+    a = []
+    occured = []
+    for i in range(n):
+        if b[i] not in occured:
+            # print(-1)
+            d[len(d)+1] = [b[i], 1]
+            a.append(len(d))
+            occured.append(b[i])
+        else:
+            for j in d.keys():
+                if d[j][0] == b[i]:
+                    if d[j][1]<=d[j][0]:
+                        # print(0)
+                        d[j][1] += 1
+                        a.append(j)
+                    else:
+                        # print(1)
+                        d[len(d)+1] = [b[i], 1]
+                        a.append(len(d))
+    #     print(d, occured)
+    # print()
+    # print()
+    # print()
+    print(a)
+
+
+# 5
+# 5
+# 2 3 3 3 2
+# 5
+# 1 1 1 1 1
+# 5
+# 5 5 5 5 5
+# 3
+# 1 2 4
+# 8
+# 1 3 2 3 2 2 2 3
