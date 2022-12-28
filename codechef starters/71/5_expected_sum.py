297de6bf8cb144db9130192e883faf0390d4de84
sanjiv kannaa jeganathan <jsanjiv2003@gmail.com>
2022-12-28 21:55:11 +0530

    update

--- codechef starters/71/5_expected_sum.py
+++ codechef starters/71/5_expected_sum.py
@@ -1,12 +1,16 @@
 from itertools import permutations as p
 
 for __ in range(int(input())):
-    n = int(input())
     a = [int(i) for i in input().split()]
+    n = a[0] + a[1]
     l = [1]*a[0]
     l.extend([0]*a[1])
-    # print(l)
+    print(list(p(l)))
+    sum_ = 0
     number_of_p = 0
     for i in list(p(l)):
         number_of_p += 1
-        
+        for j in range(n//2):
+            sum_ += int(l[j])
+    sum_ = sum_/number_of_p
+    print(int(sum_))
