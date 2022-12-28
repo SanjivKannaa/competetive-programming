d8d67d00c6e8121bb5c2228dc1604cb35583ce28
sanjiv kannaa jeganathan <jsanjiv2003@gmail.com>
2022-12-28 20:40:38 +0530

    update

--- codechef starters/71/4_pet_store.py
+++ codechef starters/71/4_pet_store.py
@@ -1,16 +1,14 @@
 for __ in range(int(input())):
     n = int(input())
-    a = [int(i) for i in input().split()]
-    b = [int(i) for i in input().split()]
-    streak = 0
-    max = 0
-    for i in range(n):
-        if (a[i]==0 or b[i]==0):
-            if max<streak:
-                max = streak
-            streak = 0;
-        else:
-            streak+=1
-    if max<streak:
-        max = streak
-    print(max)
+    l = [int(i) for i in input().split()]
+    ret="YES"
+    bruh = []
+    for i in l:
+        if i not in bruh and l.count(i)%2!=0:
+            ret = "NO"
+            break
+        bruh.append(i)
+    if ret=="YES":
+        print("YES")
+    else:
+        print("NO")
