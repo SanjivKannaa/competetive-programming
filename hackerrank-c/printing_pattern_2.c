5c9504d2dd2ffba8281f6c1aa5dbe1d93b448389
sanjiv kannaa jeganathan <jsanjiv2003@gmail.com>
2022-12-29 23:30:03 +0530

    update

---
+++ hackerrank-c/printing_pattern_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    int n;
+    scanf("%d", &n);
+  	// Complete the code to print the pattern.
+    for (int i=0;i<(2*n)-1;i++){
+        
+    }
+    return 0;
+}
--- hackerrank-c/sum-of-digits-of-a-five-digit-number.c
+++ hackerrank-c/sum-of-digits-of-a-five-digit-number.c
@@ -7,7 +7,8 @@ int main() {
 	
     int n;
     scanf("%d", &n);
-    int sum=0;
+    int sum=n%10+(n/10)%10+(n/100)%10+(n/1000)%10+(n/10000)%10;
     //Complete the code to calculate the sum of the five digits on n.
-    return 0;
+    printf("%d", sum);
+    return sum;
 }
