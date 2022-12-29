3766db6ace0cb8cbce77231135041600a70ebede
sanjiv kannaa jeganathan <jsanjiv2003@gmail.com>
2022-12-29 23:19:19 +0530

    update

---
+++ hackerrank-c/sum-of-digits-of-a-five-digit-number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int n;
+    scanf("%d", &n);
+    int sum=0;
+    //Complete the code to calculate the sum of the five digits on n.
+    return 0;
+}
