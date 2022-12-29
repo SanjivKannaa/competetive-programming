f45032e55893ec2a0085caa4f06594d618434c39
sanjiv kannaa jeganathan <jsanjiv2003@gmail.com>
2022-12-29 23:36:47 +0530

    update

--- hackerrank-c/printing_pattern_2.c
+++ hackerrank-c/printing_pattern_2.c
@@ -5,12 +5,37 @@
 
 int main() 
 {
+int n,i,j;
+scanf("%d", &n);//2
+int len=2*n-1;//3
+int start=0;
+int end=len-1;//2
+int a[len][len];//a[13][13];//a[3][3]
 
-    int n;
-    scanf("%d", &n);
-  	// Complete the code to print the pattern.
-    for (int i=0;i<(2*n)-1;i++){
-        
-    }
-    return 0;
+  while(n!=0)
+  {
+      for(i=start;i<=end;i++)
+      {
+          for(j=start;j<=end;j++)
+          {
+              if(i==start || i==end || j==start || j==end)
+              {
+                  a[i][j]=n;
+              }
+          }
+
+      }
+        start++;
+          end--;
+          n--;
+  }
+  for(i=0;i<len;i++)
+  {
+      for(j=0;j<len;j++)
+      {
+          printf("%d ",a[i][j]);
+      }
+      printf("\n");
+  }
+  return 0;
 }
